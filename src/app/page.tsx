b14e79d689589f663cf3cdf0b7cc916f840780a6
Rodrigo Félix <r.felix@campus.fct.unl.pt>
2024-09-24 23:32:19 +0100

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -172,23 +172,19 @@ export default function Page() {
             })}
           </div>
         </Section>
-
-        <Section className="print-force-new-page scroll-mb-16">
-          <h2 className="text-xl font-bold">Projects</h2>
-          <div className="-mx-3 grid grid-cols-1 gap-3 print:grid-cols-3 print:gap-2 md:grid-cols-2 lg:grid-cols-3">
-            {RESUME_DATA.projects.map((project) => {
+        <Section>
+          <h2 className="text-xl font-bold">Hobbies & Interests</h2>
+          <div className="flex flex-wrap gap-1">
+            {RESUME_DATA.hobbies.map((hobbie) => {
               return (
-                <ProjectCard
-                  key={project.title}
-                  title={project.title}
-                  description={project.description}
-                  tags={project.techStack}
-                  link={"link" in project ? project.link.href : undefined}
-                />
+                <Badge className="print:text-[10px]" key={hobbie}>
+                  {hobbie}
+                </Badge>
               );
             })}
           </div>
         </Section>
+
       </section>
 
       <CommandMenu
